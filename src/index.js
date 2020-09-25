397f1975fc5642c6cdea4a56bbe555c917f0848f
frenki1231 <70661525+frenki1231@users.noreply.github.com>
2020-09-25 20:07:08 +0300

    fix: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
+  if (array!=undefined&&array.length!=0) return Math.min(...array);
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array!=undefined&&array.length!=0) return Math.max(...array);
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array!=undefined&&array.length!=0) return array.reduce((item,sum)=>sum+item,0)/array.length;
   return 0;
 }
